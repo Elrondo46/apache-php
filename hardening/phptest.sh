098ae90f56672bf50de6757bf47369db24658a86
Elrondo46 <elrond94@hotmail.com>
2021-10-21 21:14:59 +0200

    Fix php8 hardenning

--- hardening/phptest.sh
+++ hardening/phptest.sh
@@ -16,9 +16,8 @@ if [ -z "${PHPVersion}" ]; then
       echo "Running PHPv8..."
       mkdir hardening && cd hardening
       apt-get update && apt-get install -y --no-install-recommends git=* && rm -rf /var/lib/apt/lists/* && cd / && \
-    git clone https://github.com/jvoisin/snuffleupagus && cd snuffleupagus/src && phpize && ./configure --enable-snuffleupagus && make && make install && docker-php-ext-enable snuffleupagus
-    echo "sp.configuration_file=/usr/local/etc/php/conf.d/snuffleupagus.rules" >> /usr/local/etc/php/conf.d/docker-php-ext-snuffleupagus.ini
+      git clone https://github.com/jvoisin/snuffleupagus && cd snuffleupagus/src && phpize && ./configure --enable-snuffleupagus && make && make install && docker-php-ext-enable snuffleupagus
       echo "sp.configuration_file=/usr/local/etc/php/conf.d/snuffleupagus.rules >> /usr/local/etc/php/conf.d/docker-php-ext-snuffleupagus.ini"
       wget "https://raw.githubusercontent.com/Elrondo46/apache-php/master/hardening/hard_php8.rules"
-      mv hard_php7.rules /usr/local/etc/php/conf.d/snuffleupagus.rules
+      mv hard_php8.rules /usr/local/etc/php/conf.d/snuffleupagus.rules
     fi
