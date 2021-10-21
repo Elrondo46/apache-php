3b506874f478e548625073ebfe9b088433c40b67
Elrondo46 <elrond94@hotmail.com>
2021-10-21 22:28:58 +0200

    Update phptest.sh

--- hardening/phptest.sh
+++ hardening/phptest.sh
@@ -17,7 +17,7 @@ if [ -z "${PHPVersion}" ]; then
       mkdir hardening && cd hardening
       apt-get update && apt-get install -y --no-install-recommends git=* && rm -rf /var/lib/apt/lists/* && cd / && \
       git clone https://github.com/jvoisin/snuffleupagus && cd snuffleupagus/src && phpize && ./configure --enable-snuffleupagus && make && make install && docker-php-ext-enable snuffleupagus
-      echo "sp.configuration_file=/usr/local/etc/php/conf.d/snuffleupagus.rules >> /usr/local/etc/php/conf.d/docker-php-ext-snuffleupagus.ini"
+      echo "sp.configuration_file=/usr/local/etc/php/conf.d/snuffleupagus.rules" >> /usr/local/etc/php/conf.d/docker-php-ext-snuffleupagus.ini
       wget "https://raw.githubusercontent.com/Elrondo46/apache-php/master/hardening/hard_php8.rules"
       mv hard_php8.rules /usr/local/etc/php/conf.d/snuffleupagus.rules
     fi
